e6480b0d8fd587b3bd323106f0e2189a7cea57a9
alisendiadobe <alisendiadobe@gmail.com>
2026-05-14 17:40:02 +0300

    Update app.py

--- my_app/app.py
+++ my_app/app.py
@@ -65,8 +65,8 @@ st.markdown(custom_style, unsafe_allow_html=True)
 
 # 3. Fixed Coordinates and Values (Your specific settings)
 X_CENTER = 2152
-Y_POS = 3400
-FONT_SIZE = 240
+Y_POS = 3600
+FONT_SIZE = 230
 TEXT_COLOR = "#006937"
 TEMPLATE_PATH = "my_app/template.jpg"
 FONT_PATH = "my_app/font.ttf"

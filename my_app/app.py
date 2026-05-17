1cfc2f723000510844dc57955e30b5072e8620a9
alisendiadobe <alisendiadobe@gmail.com>
2026-05-17 12:03:51 +0300

    Update app.py

--- my_app/app.py
+++ my_app/app.py
@@ -24,7 +24,7 @@ bin_str = get_base64_bin(bg_image_path)
 if bin_str:
     bg_style = f'background-image: url("data:image/jpeg;base64,{bin_str}");'
 else:
-    bg_style = 'background-color: #e8dbb9;'
+    bg_style = 'background-color: #f2e9d3;'
 
 custom_style = f"""
     <style>
